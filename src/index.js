afcc4464724a75ee5eeec509e49d91fd6f6965ca
Svetlana Shlekht <sv.2502@mail.ru>
2023-07-18 21:20:06 +0300

    feat solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    // разбор, длинно-понятно
+    // let str=String(n);
+    // let arr = str.split('');
+    // let revArr = arr.reverse();
+    // let newStr = revArr.join('');
+    // let result = parseInt(newStr);
+// коротко
+  return parseInt(String(n).split('').reverse().join(''));
 }
